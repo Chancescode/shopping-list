675c7b8edfa072ca9677c4d6b34b6d8d706d0430
Chancescode <63568305+Chancescode@users.noreply.github.com>
2020-06-08 11:06:34 -0700

    added js

---
+++ main.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('#js-shopping-list-form').submit(function(event){
+      event.preventDefault();
+      var textInput = $('#shopping-list-entry').val();
+      console.log(textInput)
+  
+      $('.shopping-list').append(
+        `<li>
+            <span class="shopping-item">${textInput}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`
+      );
+  
+      $('#shopping-list-entry').val('');
+    });
+  
+    $('.shopping-list').on('click', '.shopping-item-toggle', 
+    e => {
+      $(e.currentTarget)
+        .closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
+    });
+  
+    $('.shopping-list').on('click', '.shopping-item-delete',
+    e => {
+      $(e.currentTarget)
+        .closest('li').remove();
+    });
+  });
+  
